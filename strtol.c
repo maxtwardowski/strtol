d0c96e9c3cc2e2a7bfb2031cf274f50223ef3084
maxtwardowski <twardowskimax@gmail.com>
2018-04-16 20:25:36 +0200

    Octal recognised in '0'/'0o' notions, number terminated not only with space, other minor fixes

--- strtol.c
+++ strtol.c
@@ -1,92 +1,117 @@
-#include <assert.h> //po co ta biblioteka
-#include <ctype.h> //jw
-#include <errno.h>  //jw
-#include <limits.h> //jw
-#include <string.h> //jw
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include <errno.h>
+#include <limits.h>
 
-long strtol(char* nptr, char **endptr /*podwójny wskaŸnik?*/, int base){
-char *string;
-long number = 0;
-int sign, digit;
-unsigned long cutoff;
-int cutlim, any = 0;
-/********************* quits if string is empty *******************/
-string = nptr;
-/**************************** looks for the start of the string, if there are white spaces beforehand *******************************************/
-while(isspace(*string))
-{
-string++;
-}
-/********************* checks the sign of the number *******************/
-if(*string=='-'){
-	sign = 1;
-	string++;
-}
-else if (*string=='+'){
-		sign = 0;
-		string++;
-} else
-    sign = 0;
-/************************ Checks if base is 0 and quits if number is 0x+(>f)*******************************/
-if((base==8)&&(*string=='0')) string++;
-if((base==16)&&(*string=='0')){
-	 string++;
-	 if((*string=='x')||(*string=='X')){
-		 string++;
-		 if(*string > 'f') {string--; *endptr = string; return 0;}
-	  }
-	 else string--;
-}
-if((*string=='0')&&(base==0)){
-base = 8;
-string++;
-	if((*string=='x')||(*string=='X')){
-		 base = 16;
-		 string++;
-		 if(*string > 'f') {string--; *endptr = string; return 0;}
-	}
-	if(*string > '7') {*endptr = string; return 0;}
-}
-if(base==0) base = 10;
-if(base <2 || base >36){
-	errno=ERANGE;// o co chodzi
-	return 0;
-}
-cutoff = sign ? -(unsigned long) LONG_MIN : LONG_MAX; // jesli sign=1 to cutoff=-LONG_MIN
-cutlim = cutoff % (unsigned long) base; //cutlim to reszta z dzielenia cutoff przez baze
-cutoff /= (unsigned long) base; //cutoff=cutoff/baze
-while(*string!='\0'){ //kiedy string wskazuje na wartosc ktora nie jest koncem pliku
-	if (isdigit(*string)){ //jesli string wskazuje na liczbe, funkcja isdigit zwraca 0 jesli jej argument nie jest liczba, a jesli jest to zwraca ta liczbe
-		digit = *string - '0'; //digit=ta liczba
-	}
-	else {
-		if(isalpha(*string)){// isalpha zwraca 0 jesli argument nie jest litera, jesli jest to zwraca ta litere
-			if(islower(*string)){ //sprawdza czy arg jest ma³¹ liter¹
-				digit = (*string - 'a') + 10; // zamienia litery uzywane w systemie 16 na liczby np B=11 itp
-			}
-			else digit = (*string - 'A') + 10;
-		}
-		else{
-			break;
-		}
-	}
-	if (any < 0 || number > cutoff || (number == cutoff && (int) digit > cutlim)){//co robi ten warunek?
-		  any =-1;
-		  string++;
-	}
-	else {//?
-		any = 1;
-		number = (number*base)+digit;
-		string++;
-	}
-}
-if (any<0){
-	number = sign ? LONG_MIN : LONG_MAX;
-	errno = ERANGE;
-}
-if (sign==1){
-	number*=-1;
-}
-if (endptr != '\0') *endptr = any ? string : (char *) nptr;
-return number;
+long strtolx (const char *nPtr, char **endPtr, int base) {
+    long number = 0;
+    char * charposition;
+    int digit, sign, cutlim, any = 0;
+    const int POSITIVE = 1,
+              NEGATIVE = 0;
+    unsigned long cutoff;
+
+    assert(nPtr);
+    charposition = nPtr;
+
+    //looking for a space if the beggining of the string is moved further
+    while (isspace(* charposition))
+        charposition++;
+
+    if (* charposition == '+') {
+        sign = POSITIVE;
+        charposition++;
+    } else if (* charposition == '-') {
+        sign = NEGATIVE;
+        charposition++;
+    } else
+        sign = POSITIVE;
+
+    if ((base == 8) && (*charposition == '0'))
+        charposition++;
+    if ((base == 16) && (*charposition == '0')) {
+        charposition++;
+        if (*charposition == 'x' || *charposition == 'X') {
+            charposition++;
+            if (*charposition > 'f') {
+                charposition--;
+                *endPtr = charposition;
+                return 0;
+            }
+        }
+        else
+            charposition--;
+    }
+
+    if((* charposition == '0') && (base == 0)) {
+        base = 8;
+        charposition++;
+    	if((* charposition == 'x') || (* charposition == 'X')){
+    		 base = 16;
+    		 charposition++;
+    		 if(* charposition > 'f') {
+                 charposition--;
+                 * endPtr = charposition;
+                 return 0;
+             }
+    	}
+    	if(*charposition > '7') {
+            *endPtr = charposition;
+            return 0;
+        }
+    }
+    if(base == 0)
+        base = 10;
+    if(base < 2 || base > 36){
+    	errno = EINVAL;
+    	return 0;
+    }
+
+    if (sign)
+    	cutoff = - (unsigned long) LONG_MIN;
+    else
+    	cutoff = LONG_MAX;
+    cutlim = cutoff % (unsigned long) base;
+    cutoff /= (unsigned long) base;
+    while (* charposition != '\0') {
+    	if (isdigit(* charposition))
+    		digit = * charposition - '0';
+    	else {
+    		if(isalpha(* charposition)) {
+    			if(islower(* charposition))
+    				digit = (* charposition - 'a') + 10;
+    			else
+                    digit = (* charposition - 'A') + 10;
+    		} else
+    			break;
+    	}
+    	if (any < 0 || number > cutoff || (number == cutoff && (int) digit > cutlim)) {
+    		  any = -1;
+    		  charposition++;
+    	}
+    	else {
+    		any = 1;
+    		number = (number * base) + digit;
+    		charposition++;
+    	}
+    }
+    if (any < 0) {
+    	if (sign)
+    		number = LONG_MIN;
+    	else
+    		number = LONG_MAX;
+    	errno = ERANGE;
+    }
+    if (sign == NEGATIVE)
+    	number *= -1;
+    if (endPtr != '\0') {
+    	if (any)
+    		* endPtr = charposition;
+    	else
+    		* endPtr = (char *) nPtr;
+    }
+    return number;
 }
--- strtolwip.c
+++ strtolwip.c
@@ -2,57 +2,140 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <assert.h>
+#include <errno.h> //for returning error codes to compare with test_strtol
+#include <limits.h> //for LONG_MAX & LONG_MIN
+
+#define NUL '\0'
 
 long strtolx (const char *nPtr, char **endPtr, int base);
 
 int main(int argc, char const *argv[]) {
-    const char text[] = "0xfftu jest dalej tekst";
+    char input[] = "0o764 wat man";
     char * rest;
-    strtolx(text, &rest, 0);
+    long convertednumber = strtolx (input, &rest, 0);
+    printf("Converted number = %ld\n", convertednumber);
+    printf("Rest of the string: %s\n", rest);
 
     return 0;
 }
 
 long strtolx (const char *nPtr, char **endPtr, int base) {
+    //checking if the base value is correct
+    if((base < 2 || base > 36) && base != 0) {
+        errno = EINVAL;
+        return 0;
+    }
+
     long number = 0;
-    char * charposition;
-    int sign;
+    char * divider;
+    int currentdigit, sign, cutlim, any = 0;
     const int POSITIVE = 1,
               NEGATIVE = 0;
-    assert(nPtr);
-    charposition = nPtr;
+    unsigned long cutoff;
+
+    divider = nPtr;
 
     //looking for a space if the beggining of the string is moved further
-    while (isspace(* charposition))
-        charposition++;
+    while (isspace(* divider))
+        divider++;
 
-    if (* charposition == '+') {
+    //detecting the sign, positive by default
+    if (* divider == '+') {
         sign = POSITIVE;
-        charposition++;
-    } else if (* charposition == '-') {
+        divider++;
+    } else if (* divider == '-') {
         sign = NEGATIVE;
-        charposition++;
+        divider++;
     } else
         sign = POSITIVE;
 
-    if ((base == 8) && (*charposition == '0'))
-        charposition++;
-    if ((base == 16) && (*charposition == '0') {
-        charposition++;
-        if (*charposition == 'x' || *charposition == 'X') {
-            charposition++;
-            if (*charposition > 'f') {
-                charposition--;
-                *endptr = charposition;
+    if ((base == 8) && (*divider == '0')) {
+        divider++;
+        if (*divider == 'o') //if the input includes 'o', it's skipped
+            divider++;
+    }
+    else if ((base == 16) && (*divider == '0')) {
+        divider++;
+        if (*divider == 'x' || *divider == 'X') {
+            divider++;
+            if (*divider > 'f') {
+                divider--;
+                *endPtr = divider;
                 return 0;
             }
         }
         else
-            charposition--;
+            divider--;
+    //basically the system-detecting algorithm
+    } else if (base == 0) {
+        if (* divider == '0') {
+            divider++;
+            if (* divider != 'x' && * divider != 'X') {
+                if (* divider == 'o')
+                    divider++;
+                base = 8;
+            } else if (* divider == 'x' || * divider == 'X') {
+                base = 16;
+                divider++;
+                if (* divider > 'f') {
+                    divider--;
+                    * endPtr = divider;
+                    return 0;
+                }
+            }
+        } else if (* divider >= '1' && * divider <= '9') {
+                base = 10;
+                divider++;
+        }
     }
 
-
-
+    //two conditions just for clarity --> |LONG_MIN| = LONG_MAX + 1
+    if (sign)
+        cutoff = LONG_MAX / (unsigned long) base;
+    else
+        cutoff = (unsigned long) LONG_MIN / (unsigned long) base;
+    cutlim = cutoff % (unsigned long) base;
+    while (* divider != NUL) { //looping until the end of the input string
+    	if (iscurrentdigit(* divider))
+    		currentdigit = * divider - '0'; //converting to the actual integer
+    	else {
+    		if(isalpha(* divider)) {
+    			if(islower(* divider) && (* divider - 'a') + 10 < base)
+    				currentdigit = (* divider - 'a') + 10;
+    			else if (!islower(* divider) && (* divider - 'A') + 10 < base)
+                    currentdigit = (* divider - 'A') + 10;
+                else
+                    break;
+    		} else
+    			break;
+    	}
+    	if (any < 0 ||
+            number > cutoff ||
+            (number == cutoff && (int) currentdigit > cutlim)) {
+    		  any = -1;
+    		  divider++;
+    	} else { //the actual conversion to decimal
+    		any = 1;
+    		number = (number * base) + currentdigit;
+    		divider++;
+    	}
+    }
+    if (any < 0) {
+    	if (sign)
+    		number = LONG_MIN;
+    	else
+    		number = LONG_MAX;
+    	errno = ERANGE;
+    }
+    if (sign == NEGATIVE)
+    	number *= -1;
+    if (endPtr != '\0') {
+        if (isspace(* divider)) //checking if the number is separated
+            divider++;          //from the rest of the string
+    	if (any)
+    		* endPtr = divider;
+    	else
+    		* endPtr = (char *) nPtr;
+    }
     return number;
 }
